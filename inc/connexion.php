734c399fa32d518cd92eeb72dd1c6f741f0eb8ef
Lionel <zeadkiel13@gmail.com>
2018-03-02 13:21:14 +0100

    connexion à la bdd avec new pdo

---
+++ inc/connexion.php
@@ -0,0 +1,7 @@
+<?php
+$dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+$pdo = new PDO($dsn, $login, $pwd, [
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
