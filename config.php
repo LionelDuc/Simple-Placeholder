a7a1aeee7cd6eaa94166fc7ec03c864b129ea671
Lionel <zeadkiel13@gmail.com>
2018-03-02 13:10:35 +0100

    variable de connection à la bdd

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('THEME', 'theme/');
+
+$dbname = 'meteo';
+$host = 'localhost';
+$login = 'best-root';
+$pwd = 'sudo-root';
+
+
+//pour que vous puissez voir un quelconque resultat renseigner correctement le login et le pwd de votre bdd
+//avec php my admin par exemple installer la base sql.
