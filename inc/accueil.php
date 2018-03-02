9eb553aecfd48e3474cb20d89cac165bc9db0d80
Lionel <zeadkiel13@gmail.com>
2018-03-02 13:16:07 +0100

    fonction affichage de contenu d'appel de donner à la base

--- inc/accueil.php
+++ inc/accueil.php
@@ -13,3 +13,17 @@ switch ($action){
         bulletinDetail();
         break;
 }
+
+function bulletinAction(){
+    global $pdo;
+
+    $sql = 'SELECT l.id, ville, meteo,contenu,date FROM bulletin JOIN temps t ON bulletin.temps_id= t.id JOIN lieu l ON l.id = bulletin.lieu_id ORDER BY date DESC ';
+    $result = $pdo->query($sql);
+    $bulletins =$result->fetchAll();
+    include THEME .'vue/accueil.html.php';
+    var_dump($bulletins);
+}
+
+function bulletinDetail(){
+
+}
